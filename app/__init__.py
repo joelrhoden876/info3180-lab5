89ed2c5e2b6052d120367c77e170c198a2867d7f
Joel Rhoden <joelrhodenjm@gmail.com>
2023-04-11 21:54:58 -0500

    creating movies table unsuccessful

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,20 @@
 from flask import Flask
+
+from flask_sqlalchemy import SQLAlchemy
 from .config import Config
+# import flask migrate here
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'some$3cretKey'
 app.config.from_object(Config)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'driver://lab5:UWIMonaLab5@localhost/lab5'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+# Instantiate Flask-Migrate library here
 
-from app import views
+migrate = Migrate(app, db) 
+
+
+from app import views, models
